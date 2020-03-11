2dd0438d52fb37a5c23f9065a0abc6fc4955fb35
Benno Dreißig <x3@x3cion.net>
2020-03-11 22:24:33 +0000

    More linting, less noise

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src", "<rootDir>/test"],
-  coverageDirectory: "coverage/",
-  moduleDirectories: ["node_modules", "src"]
+	preset: "ts-jest",
+	testEnvironment: "node",
+	roots: ["<rootDir>/src", "<rootDir>/test"],
+	coverageDirectory: "coverage/",
+	moduleDirectories: ["node_modules", "src"]
 };
